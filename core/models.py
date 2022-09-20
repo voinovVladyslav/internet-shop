986e37b6d90950f459e393bb204b3955b448d7b8
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-09-20 20:58:11 +0300

    create category model + pillow for images

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
