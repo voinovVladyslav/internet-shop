befcc9d723e321d072cc8d4dcd0155d300175df4
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-09-20 21:43:23 +0300

    change home page to categories

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from django.views import generic
+
+from core.models import Category
 
 
 class HomeView(generic.TemplateView):
-    template_name = 'core/home.html'
+    template_name = 'core/categories.html'
+    def get_context_data(self, **kwargs: any) -> dict[str, any]:
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
