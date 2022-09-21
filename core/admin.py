e7bbe77edbce13b1b71fd2e8a12ad3166a135760
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-09-21 10:43:48 +0300

    create item model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from core.models import Category
+from core.models import Category, Item
 
 
 admin.site.register(Category)
+admin.site.register(Item)
--- core/models.py
+++ core/models.py
@@ -7,3 +7,15 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ManyToManyField('Category')
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
