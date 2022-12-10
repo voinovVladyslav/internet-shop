744f6ef3bc2d57340d56523d781a22d07a224c34
Voinov Vladyslav <voinovvladyslav@gmail.com>
2022-12-10 11:00:23 +0200

    bug fix

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from core.models import Category, Item
 
 class HomeView(generic.TemplateView):
     template_name = 'core/categories.html'
-    def get_context_data(self, **kwargs: any) -> dict[str, any]:
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         return context
